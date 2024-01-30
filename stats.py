497d9d8b7f962e3d17f1331c7a7a13d0ec7e4664
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-01-30 18:24:12 +0100

    ultimo

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 import os
 import json
 from collections import Counter
-import csv
+import numpy as np
 import time
 
 # Definisci le variabili per i dati da raccogliere
@@ -13,9 +13,16 @@ num_articles_without_title = 0
 num_articles_without_abstract = 0
 num_articles_without_id = 0
 all_keywords = []
+no_abstract=[]
+no_id=[]
+num_tables_list=[]
+num_figures_list=[]
 keyword_counter = Counter()
 articles_per_table = Counter()
 articles_per_figure = Counter()
+tables_to_id={}
+
+
 
 # Inizializza il timer
 start_time = time.time()
@@ -30,21 +37,26 @@ for filename in os.listdir(json_folder):
 
             # Conta il numero di tabelle e celle
             num_tables += len(data['content']['tables'])
-            for table in data['content']['tables']:
-                num_cells += len(table['cells'])
-            articles_per_table[len(table['cells'])] += 1
-                
+            #for table in data['content']['tables']:
+                #num_cells += len(table['cells'])
+            articles_per_table[len(data['content']['tables'])] += 1
+             
             # Conta il numero di immagini
             num_figures += len(data['content']['figures'])
             articles_per_figure[len(data['content']['figures'])] += 1
+            
+            num_tables_list.append(len(data['content']['tables']))
+            num_figures_list.append(len(data['content']['figures']))
             
             # Verifica la presenza di titolo, abstract e id
             if not data['content']['title']:
                 num_articles_without_title += 1
             if not data['content']['abstract']:
                 num_articles_without_abstract += 1
+                no_abstract.append(filename)
             if not data.get('pmcid'):
                 num_articles_without_id += 1
+                no_id.append(filename)
 
             # Aggiungi le keywords alla lista globale e al counter
             all_keywords.extend(data['content']['keywords'])
@@ -67,7 +79,16 @@ avg_cells_per_table = num_cells / num_tables if num_tables > 0 else 0
 avg_keywords_per_article = len(all_keywords) / num_files if num_files > 0 else 0
 avg_figures_per_article = num_figures / num_files if num_files > 0 else 0
 
-# Stampa i risultati
+median_tables = np.median(num_tables_list)
+median_figures = np.median(num_figures_list)
+
+print(f"Mediana delle tabelle: {median_tables}")
+print(f"Mediana delle figure: {median_figures}")
+
+#print("No abstract: ",no_abstract)
+#print("No id",no_id)    
+
+""" # Stampa i risultati
 print(f"Numero di file: {num_files}")
 print(f"Numero di tabelle: {num_tables}")
 print(f"Numero di immagini: {num_figures}")
@@ -80,7 +101,7 @@ print(f"Numero di articoli senza abstract: {num_articles_without_abstract}")
 print(f"Numero di articoli senza id: {num_articles_without_id}")
 
 # Keyword più ricorrenti
-most_common_keywords = keyword_counter.most_common(5)
+most_common_keywords = keyword_counter.most_common(20)
 print(f"\nKeyword più ricorrenti:")
 for keyword, count in most_common_keywords:
     print(f"{keyword}: {count}")
@@ -97,6 +118,6 @@ for num_tables, count in sorted_articles_per_table:
 print("\nNumero di articoli per numero di figure (ordinato per numero di articoli):")
 sorted_articles_per_figure = sorted(articles_per_figure.items(), key=lambda x: x[1], reverse=True)
 for num_figures, count in sorted_articles_per_figure:
-    print(f"{num_figures} figure: {count} articoli")
+    print(f"{num_figures} figure: {count} articoli") """
     
     
--- wc.py
+++ wc.py
@@ -2,13 +2,25 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # Frequenza delle keywords
-keyword_frequencies = {"Children": 110, "Malaria": 109, "Epidemiology": 103,
-                       "Quality of life": 98, "Obesity": 95, "HIV": 91,
-                       "Breast cancer": 90, "Physical activity": 85,
-                       "Prognosis": 84, "Mortality": 79}
+keyword_frequencies = {    
+    "Obesity": 462,
+    "Children": 435,
+    "HIV": 424,
+    "Epidemiology": 417,
+    "Breast cancer": 386,
+    "Quality of life": 375,
+    "Malaria": 374,
+    "Physical activity": 370,
+    "Prognosis": 358,
+    "Mortality": 352,
+    "Prevalence": 338,
+    "prognosis": 329,
+    "Risk factors": 317,
+    "Depression": 297,
+    "Ethiopia": 261,}
 
 # Creare il word cloud direttamente dalla frequenza delle parole
-wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(keyword_frequencies)
+wordcloud = WordCloud(width=1600, height=800, background_color="white").generate_from_frequencies(keyword_frequencies)
 
 # Visualizzare il word cloud utilizzando matplotlib
 plt.figure(figsize=(10, 5))
